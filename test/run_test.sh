d0f45529d589143d9c0960e448f9d7cc0933979c
Dov Murik <dmurik@us.ibm.com>
2015-10-27 14:46:17 -0400

    test: extract each test to its own file under test/tests; valgrind support

--- test/run_test.sh
+++ test/run_test.sh
@@ -17,6 +17,7 @@ OPTIONS:
    -k      kill processes using pkill
    -s      skip log (keep stderr)
    -t      skip test client/server log (keep test client/server stderr)
+   -v      run goldy under valgrind
 EOF
 
 }
@@ -25,8 +26,9 @@ keep_log_files=0
 pkill_processes=0
 keep_stderr=0
 keep_test_stderr=0
+use_valgrind=0
 
-while getopts "klst" OPTION
+while getopts "klstv" OPTION
 do
   case $OPTION in
     l)
@@ -44,6 +46,10 @@ do
     t)
       keep_test_stderr=1
       echo "Will not capture stderr for test client(s)"
+      ;;
+    v)
+      use_valgrind=1
+      echo "Will run goldy under valgrind"
       ;;
     h)
       usage
@@ -110,30 +116,49 @@ log_goldy_mem_usage() {
   if [ -z "$goldypid" ] ; then
     return
   fi
-  log "goldy (PID $goldypid) resource usage (RSS VSZ %CPU): $(ps -p $goldypid -o rss= -o vsz= -o %cpu=)"
+  m=( $(ps -p $goldypid -o rss= -o vsz=) )
+  udp_stats="-"
+  if [ -f /proc/$goldypid/net/sockstat ] ; then
+    udp_stats="$(grep ^UDP: /proc/$goldypid/net/sockstat)"
+  fi
+  log "goldy (PID $goldypid) resource usage: Memory RSS=${m[0]} KB, Memory VSZ=${m[1]} KB, $udp_stats"
 }
 
 now_ms() {
   date +'%s-%N' | sed -e 's/^\([0-9]*\)-\([0-9][0-9][0-9]\).*$/\1\2/'
 }
 
-run_concurrent_client_scenarios() {
-  testnum=$1
-  description=$2
-  duplicates=$3
-  clients_scenarios=${@:4}
-  echo "=============================================== concurrent scenarios"
+run_test_file() {
+  filename=$1
+  testnum=$(basename $filename)
+  unset DESCRIPTION
+  unset DUPLICATES
+  for v in ${!CLIENT_*} ; do
+    unset $v
+  done
+
+  . $filename
+
+  if [ -z "$DUPLICATES" ] ; then
+    DUPLICATES=1
+  fi
+
+  log "=============================================== concurrent scenarios"
   log_goldy_mem_usage
   starttime=$(now_ms)
   pids=""
-  for i in $(eval echo "{1..$duplicates}") ; do
-    for scenario in $clients_scenarios ; do
-      if [ $keep_test_stderr == 1 ]; then
-        test/dtls_test_client -n goldy.local -h $GOLDYHOST -p $GOLDYPORT -s "$scenario" &
-      else
-        test/dtls_test_client -n goldy.local -h $GOLDYHOST -p $GOLDYPORT -s "$scenario" >> test/log/test_client.log 2>&1 &
+
+  for d in $(eval echo "{1..$DUPLICATES}") ; do
+    for v in ${!CLIENT_*} ; do
+      scenario="${!v}"
+      if [ -n "$scenario" ] ; then
+        if [ $keep_test_stderr == 1 ]; then
+          test/dtls_test_client -n goldy.local -h $GOLDYHOST -p $GOLDYPORT -s "$scenario" &
+        else
+          test/dtls_test_client -n goldy.local -h $GOLDYHOST -p $GOLDYPORT -s "$scenario" >> test/log/test_client.log 2>&1 &
+        fi
+        pids="$pids $!"
       fi
-      pids="$pids $!"
     done
   done
 
@@ -147,26 +172,31 @@ run_concurrent_client_scenarios() {
       exitcode=1
     fi
   done
-  log_goldy_mem_usage
-  echo "=============================================== results"
+
   endtime=$(now_ms)
-  log "Test $testnum took $((endtime - starttime)) milliseconds"
+  log_goldy_mem_usage
+  log "=============================================== results"
+  log "$testnum took $((endtime - starttime)) milliseconds"
   if [ $exitcode != 0 ] ; then
-    echo "not ok $testnum - $description"
+    echo "not ok $testnum - $DESCRIPTION"
     return 1
   fi
-  echo "ok $testnum - $description"
+  echo "ok $testnum - $DESCRIPTION"
   return 0
 }
 
 trap handle_signal INT TERM HUP
 
 log "Starting goldy..."
-redirect_line=""
+valgrind_cmd=""
+if [ $use_valgrind == 1 ] ; then
+  valgrind_cmd="valgrind --leak-check=full --show-reachable=yes -v"
+fi
+goldy_cmdline="./goldy -g DEBUG -l $GOLDYHOST:$GOLDYPORT -b $BACKENDHOST:$BACKENDPORT -c test/keys/test-proxy-cert.pem -k test/keys/test-proxy-key.pem"
 if [ $keep_stderr == 1 ]; then
-  ./goldy -g DEBUG -l $GOLDYHOST:$GOLDYPORT -b $BACKENDHOST:$BACKENDPORT -c test/keys/test-proxy-cert.pem -k test/keys/test-proxy-key.pem &
+  $valgrind_cmd $goldy_cmdline &
 else
-  ./goldy -g DEBUG -l $GOLDYHOST:$GOLDYPORT -b $BACKENDHOST:$BACKENDPORT -c test/keys/test-proxy-cert.pem -k test/keys/test-proxy-key.pem > test/log/goldy.log 2>&1 &
+  $valgrind_cmd $goldy_cmdline > test/log/goldy.log 2>&1 &
 fi
 
 goldypid=$!
@@ -183,50 +213,10 @@ log "Waiting for goldy and backend server to start..."
 sleep 1
 
 failures=0
-
-# Note: in order to avoid bash quoting hell, we're avoiding spaces in the
-# client scenarios. This means that each argument ("word") is one scenario
-# which will be run in its own client.
-
-many_spaces=$(printf '%1200s' | tr ' ' '_') # 1200 underscores
-big_packet="A${many_spaces}Z"
-four_packets="ABCDEF,sleep=50,serverdelay=200_GHIJKL,sleep=50,MNOPQRS,sleep=50,serverdelay=200_TUVWXYZ"
-three_big_packets="$big_packet,sleep=100,serverdelay=200_$big_packet,sleep=100,$big_packet"
-
-# Output test results in TAP format
-echo "1..9"
-
-run_concurrent_client_scenarios 1 "Small packet 1" 1 "A"
-failures=$((failures+$?))
-
-run_concurrent_client_scenarios 2 "Small packet 2" 1 "Please_reverse_this_message_body"
-failures=$((failures+$?))
-
-run_concurrent_client_scenarios 3 "Medium packet" 1 "123456789012345678901234567890123456789012345678901234567890"
-failures=$((failures+$?))
-
-run_concurrent_client_scenarios 4 "Big packet" 1 "$big_packet"
-failures=$((failures+$?))
-
-run_concurrent_client_scenarios 5 "4 small sequential packets" 1 \
-  "ABCDEF,sleep=100,GHIJKL,sleep=200,MNOPQRS,sleep=100,TUVWXYZ"
-failures=$((failures+$?))
-
-run_concurrent_client_scenarios 6 "3 parallel client with 4 distinct small packets" 1 \
-  "ABCDEF,sleep=100,GHIJKL,sleep=200,MNOPQRS,sleep=100,TUVWXYZ" \
-  "123456,sleep=100,7890AB,sleep=100,CDEFGHI,sleep=150,JKLMNOP" \
-  "ZYXWVU,sleep=50,TSRQPO,sleep=200,NMLKJIH,sleep=150,GFEDCBA"
-failures=$((failures+$?))
-
-run_concurrent_client_scenarios 7 "10 identical parallel clients with 4 small packets each" 10 "$four_packets"
-failures=$((failures+$?))
-
-run_concurrent_client_scenarios 8 "4 small clients and 4 big clients (all in parallel)" 4 \
-  "repeat=5,$four_packets" "repeat=5,$three_big_packets"
-failures=$((failures+$?))
-
-run_concurrent_client_scenarios 9 "50 identical parallel clients with 4 small packets each" 50 "$four_packets"
-failures=$((failures+$?))
+for f in test/tests/test-* ; do
+  run_test_file $f
+  failures=$((failures+$?))
+done
 
 cleanup
 
